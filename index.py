fbb75fec3421f9a4e8864652a1268d0b6b766c94
KiriJB <164177772+KiriJB@users.noreply.github.com>
2024-04-17 10:50:48 +0100

    index.py updated display geotiff
    Code updated to display a dem geotiff image using GDAL  and matplotlib libraries

--- index.py
+++ index.py
@@ -1,3 +1,23 @@
 from osgeo import gdal
+import matplotlib.pyplot as plt
+
+#open geotiff and read contents into 2d array
+tif_file = 'data\ASTDTM_Mosaic.tif'
+dataset = gdal.Open(tif_file)
+
+band = dataset.GetRasterBand(1) #assuming one band
+elevation_data = band.ReadAsArray()#read band data into an array
+print('elevation_data=',elevation_data)
+transform = dataset.GetGeoTransform()
+
+#display the geotiff image
+plt.figure(figsize=(6, 6))
+plt.imshow(elevation_data, extent=(transform[0], transform[0] + elevation_data.shape[1] * transform[1],
+                                   transform[3] + elevation_data.shape[0] * transform[5], transform[3]))
 
 
+plt.title('Geotiff Visualization')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+
+plt.show()
