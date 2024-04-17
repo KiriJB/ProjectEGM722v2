ec8456ddba03be12114060fdf925940325536d28
KiriJB <164177772+KiriJB@users.noreply.github.com>
2024-04-17 10:55:38 +0100

    update look of geotiif display
    change color map used and add color bar

--- index.py
+++ index.py
@@ -11,13 +11,13 @@ print('elevation_data=',elevation_data)
 transform = dataset.GetGeoTransform()
 
 #display the geotiff image
-plt.figure(figsize=(6, 6))
+plt.figure(figsize=(8, 6))
 plt.imshow(elevation_data, extent=(transform[0], transform[0] + elevation_data.shape[1] * transform[1],
-                                   transform[3] + elevation_data.shape[0] * transform[5], transform[3]))
+                                   transform[3] + elevation_data.shape[0] * transform[5], transform[3]), cmap='terrain')
 
 
 plt.title('Geotiff Visualization')
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
-
+plt.colorbar(label='Elevation (m)', fraction=0.046, pad=0.04)
 plt.show()
