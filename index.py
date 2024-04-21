8a646e38fe51c2eaab96baef8a36c9f62799eee9
KiriJB <164177772+KiriJB@users.noreply.github.com>
2024-04-21 18:52:28 +0100

    Add Shapefiles for testing, add function to check input is int
    1.  Add polygon shapefiles into the data folder to be used for testing the system
    2. Add function check_integer(), to check the value entered for number of points in the elevation profile is a positive integer.

--- index.py
+++ index.py
@@ -13,30 +13,36 @@ import os
 
 '''upload a polygon shapefile to obtain boundary coordinates to search the earth access database with
 to download DEM data for the area within those bounds'''
+
+
 def get_dem_data():
-    
     try:
 
-        boundary = gpd.read_file('Data\world-administrative-boundaries.shp')
+        #boundary = gpd.read_file('Data\world-administrative-boundaries.shp') # liechtenstein
+        #boundary = gpd.read_file('Data\\Andorra_Country_Boundary.shp') # Andorra
+        #boundary = gpd.read_file('Data\\Counties.shp') # Northern Ireland
+        boundary = gpd.read_file('Data\\Djibouti_Country_Boundary.shp') # Djibouti
+
         boundary = boundary.to_crs(epsg=4326)
-    
+
         # gets a single polygon (or multipolygon) composed of the individual polygons
         outline = boundary['geometry'].unary_union
         # gets the minimum rotated rectangle that covers the outline
         search_area = outline.minimum_rotated_rectangle
         search_area = shp.geometry.polygon.orient(search_area, sign=1)  # a sign of 1 means oriented counter-clockwise
-    
+
         earthaccess.login(strategy='netrc')
         # search for datasets that match the keyword 'elevation'
         # search for datasets that intersect Northern Ireland
         datasets = earthaccess.search_datasets(keyword='aster elevation',
                                                polygon=search_area.exterior.coords)
-    
+
         dataset = datasets[0]  # get the first result
         dataset.get_umm('EntryTitle')  # fill this in with the metadata field that you want
-    
-        ds_name = dataset.get_umm('ShortName')  # fill in the following with the correct field name to return the short name of the dataset
-    
+
+        ds_name = dataset.get_umm(
+            'ShortName')  # fill in the following with the correct field name to return the short name of the dataset
+
         # search for ASTER GDEM v3 granules
         # search for images that intersect our search_area
         # only show the first 10 results
@@ -45,26 +51,28 @@ def get_dem_data():
 
         length = len(results)
         granule = next(iter(results))  # get the "first" item from the list
-        
-        #Create new folder to store DEM csv, png and TIF files
+
+
+
+        # Create new folder to store DEM csv, png and TIF files
         f_name = input("Enter name for folder, csv, png and tif files:")
         f_name = str(f_name)
         os.makedirs(f_name, exist_ok=True)
         # download each of the granules to the aster_gdem directory
         downloaded_files = earthaccess.download(results, f_name)
-    
-        dem_files = [fn for fn in downloaded_files if 'dem.tif' in fn]  # use list
 
+        dem_files = [fn for fn in downloaded_files if 'dem.tif' in fn]  # use list
+        print('dem_files=', dem_files)
         # comprehension to select only filenames that match '*dem.tif'
         if len(dem_files) > 1:
-            #save mosaiced tif
+            # save mosaiced tif
             rio.merge.merge(dem_files, dst_path=f_name + '//ASTDTM_Mosaic.tif')
         else:
             if not Path(f_name + '//ASTDTM_Mosaic.tif').is_file():
                 os.rename(str(dem_files[0]), f_name + '//ASTDTM_Mosaic.tif')
 
         return f_name
-     
+
     except Exception as e:
         print("An error occurred in the get_dem_data function:", e)
 
@@ -72,8 +80,10 @@ def get_dem_data():
 # Read elevation data from a GeoTIFF file.
 def get_elevation_data(tif_file1):
     try:
-        #dataset = gdal.Open(tif_file1)
-        dataset = gdal.Open("LIE_Data\ASTDTM_Mosaic.tif")
+        print('tif_file1=', tif_file1)
+        dataset = gdal.Open(tif_file1)
+        #dataset = gdal.Open("LIE_Data\ASTDTM_Mosaic.tif")
+
         band = dataset.GetRasterBand(1)  # assuming one band
         elevation_data_temp = band.ReadAsArray()  # read band data into an array
         transform_temp = dataset.GetGeoTransform()  # convert from row/column data to co-ords.
@@ -137,7 +147,8 @@ def display_tiff(transform1, elevation_data1):
         plt.xlabel('Longitude', fontweight='bold')
         plt.ylabel('Latitude', fontweight='bold')
         plt.colorbar(label='Elevation (m)', shrink=0.7)
-        cid = plt.gcf().canvas.mpl_connect('button_press_event', lambda event: onclick(event, elevation_data1, transform1))
+        cid = plt.gcf().canvas.mpl_connect('button_press_event',
+                                           lambda event: onclick(event, elevation_data1, transform1))
 
         cursor = Cursor(plt.gca(), useblit=True, color='red', linewidth=1)
         print("Click 2 points on the map to draw a line for the elevation profile.")
@@ -147,17 +158,35 @@ def display_tiff(transform1, elevation_data1):
         print('An error occurred in the display_tiff function.', e)
 
 
+
+def check_integer():
+    while True:
+        num_points = input("Enter the number of points to use in elevation profile. \n"
+                           "The higher the number the more detailed the profile:")
+        try:
+            val = int(num_points)
+            if val < 0:  # if not a positive int print message and ask for input again
+                print("Sorry, input must be a positive integer, try again")
+                continue
+            break
+
+        except ValueError:
+            print("That's not an int!")
+
+    return val
+
 '''Interpolate elevation data for x points between the 2 start and end points, number of points is a variable,
     more points means a more accurate elevation profile'''
+
+
 def interpolate_elevation(elevation_data1, transform1, point1, point2):
     try:
         x1, y1 = point1  # start
         x2, y2 = point2  # end
 
-        num_points = input("Enter the number of points to use in elevation profile. \n"
-                           "The higher the number the more detailed the profile:")
-        num_points = int(num_points)
-        print('num points=', num_points)
+
+        num_points = check_integer()
+        #print('num points=', num_points)
 
         # find distance between the 2 points and split into equal sections
         # to find equidistant height values along the cross-section line
@@ -216,6 +245,7 @@ def convert_distance_to_metres(point_lat1, point_lon1, elevation_profile2):
     except Exception as e:
         print('An error occurred in the convert_distance_to_metres function.', e)
 
+
 # function to create a csv file containing distance from start point and height
 def create_csv(gdf_pcs_copy1):
     try:
@@ -242,6 +272,9 @@ def display_elevation_profile(gdf_pcs_copy, point1, point2, min_height, max_heig
     except Exception as e:
         print('An error occurred in the display_elevation_profile function.', e)
 
+
+# Enable GDAL exceptions handling
+gdal.UseExceptions()
 
 f_name = get_dem_data()
 
